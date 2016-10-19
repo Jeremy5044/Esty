d1cedb1b87f46c487caf353e84789b4c724067fa
Jeremy Springer <Jeremyspringer50@gmail.com>
2016-10-19 02:53:27 -0400

    addwd finished files

--- main.js
+++ main.js
@@ -1,6 +1,61 @@
-function container(){
-var arr=document.getElementByClassName("space");
-for(var x=0; x,arr.length;x++){
-	arr[x].innerHTML= $(".container");
-   }
+
+console.log(data);
+
+
+var results = data.results
+
+console.log(results)
+
+results.forEach(renderInput);
+
+function renderInput (object) {
+	console.log(object)
+
+var img_url = object.Images[0].url_170x135
+
+var title= object.title;
+
+var price= object.price;
+
+var url=object.url;
+
+var shopName=object.Shop.shop_name;
+
+	var html = `
+
+<div class="box1">
+
+
+<div>
+       <a href="${url}"><img src= "${img_url}"></a>
+
+  <div class="box2">
+   <div class="title"> ${title}</div>
+   
+  <div class="float">
+  <div class="shop">${shopName}
+  <div class= "move">
+	   
+	  
+
+<span class="money"> $${price}</span>
+
+
+ 
+  </div>
+	   </div>
+	
+
+ </div>`;
+	$(".box").append(html);
+}
+renderInput()
+function title(top){
+console.log(top)
+
+var html = `
+<h3 class="top">Top categories for “whiskey”
+                    </h3>`;
+$(".tit").append(html);
+
 }
