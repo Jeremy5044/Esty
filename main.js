80f30980ce17bd992029effdd72db072c3cc2372
Jeremy Springer <Jeremyspringer50@gmail.com>
2016-10-21 03:47:07 -0400

    added  all files to page

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 
 console.log(data);
 
-
 var results = data.results
 
 console.log(results)
@@ -11,57 +10,45 @@ results.forEach(renderInput);
 function renderInput (object) {
 	console.log(object)
 
-var img_url = object.Images[0].url_170x135
-
-var title= object.title;
-
-var price= object.price;
-
-var url=object.url;
-
-var shopName=object.Shop.shop_name;
+  var img_url = object.Images[0].url_170x135
+  var title= object.title;
+  var price= object.price;
+  var url=object.url;
+  var shopName=object.Shop.shop_name;
 
 	var html = `
+    <div class="box1">
 
+    <div>
+    <div class="hambox">
+    <div class= "pic"><img class="ham" src ="./images/hamburger.png"> </div>
+    <div class="pic2"><img class="ham2"src="./images/heart.png"></div>
+    </div>
+    <a href="${url}"><img src= "${img_url}"></a>
 
-<div class="box1">
+    <div class="box2">
+      <a href="${url}"><div class="title"> ${title}</div></a>
+       
+    <div class="float">
+    <div class="shop">${shopName}
+    <div class= "move">
+    <span class="money"> $${price}</span>
+     
+    </div>
+    </div>
+    	
+     </div>`;
 
-
-<div>
-<div class="hambox">
-<div class= "pic"><img class="ham" src ="./images/hamburger.png"> </div>
-<div class="pic2"><img class="ham2"src="./images/heart.png"></div>
-</div>
-
-       <a href="${url}"><img src= "${img_url}"></a>
-
-  <div class="box2">
-   <div class="title"> ${title}</div>
-   
-  <div class="float">
-  <div class="shop">${shopName}
-  <div class= "move">
-	   
-	  
-
-<span class="money"> $${price}</span>
-
-
- 
-  </div>
-	   </div>
-	
-
- </div>`;
 	$(".box").append(html);
 }
-renderInput()
-function title(top){
-console.log(top)
 
-var html = `
-<h3 class="top">Top categories for “whiskey”
-                    </h3>`;
-$(".tit")
+// renderInput()
+// function title(top){
+// console.log(top)
 
-}
+// var html = `
+// <h3 class="top">Top categories for “whiskey”
+//                     </h3>`;
+// $(".tit")
+
+// }
